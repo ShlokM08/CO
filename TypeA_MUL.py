8034b91b5745583f8f8551124b94bfc95319e1ad
sejalkhurana21418 <sejal21418@iiitd.ac.in>
2022-07-01 21:08:28 +0530

    enter

---
+++ TypeA_MUL.py
@@ -0,0 +1,9 @@
+import DICT_VALUE as l
+import Unused_bit as U
+l.op_code
+user=[x for x in input().split()]
+val=l.op_code["mul"]
+valueR1=l.op_code[user[1]]
+valueR2=l.op_code[user[2]]
+valueR3=l.op_code[user[3]]
+print(val+U.unused["A"]+valueR1+valueR2+valueR3)
