0debc1a8ff6bcce0d7ea7686f7434f6a7d9e54ae
ShlokM08 <108210012+ShlokM08@users.noreply.github.com>
2022-07-02 02:50:48 +0530

    enter

--- TYPE_B_RIGHTSHIFT.py
+++ TYPE_B_RIGHTSHIFT.py
@@ -1,6 +1,7 @@
+
 import DICT_VALUE as value
-#add R1 SImm
-#mov R1 $10
+#rs R1 SImm
+#rs R1 $10
 #opcode=5bits reg1=3bits Immediate Valye=8bits 
 to_encode=[x for x in input().split()]
 x=to_encode[2].split("$")
@@ -13,11 +14,10 @@ while len(x) < 8:
     x += '0'
 bnr = x[::-1]
 
-def move_R(to_encode):
+def rs(to_encode):
     binary_encoding=""
-    if to_encode[0]=="mov":
-        binary_encoding=value.op_code["mov_R"]
+    if to_encode[0]=="rs":
+        binary_encoding=value.op_code["rs"]
     binary_encoding+=value.op_code[to_encode[1]]+bnr
     print(binary_encoding)
-move_R(to_encode)
-#opcode=5bits reg1=3bits Immediate Valye=8bits
+rs(to_encode)
