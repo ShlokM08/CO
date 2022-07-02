49b292766a80af7dbb07e5384552429f25766237
sejalkhurana21418 <108174729+sejalkhurana21418@users.noreply.github.com>
2022-07-03 02:00:33 +0530

    enter

--- TYPE_B_LEFTSHIFT.py
+++ TYPE_B_LEFTSHIFT.py
@@ -2,28 +2,31 @@ import DICT_VALUE as l
 import Unused_bit as U
 
 user=[x for x in input().split()]
-val=l.op_code["ls"]
-valueR1=l.op_code[user[1]]
-valueR2=user[2][1:]
-x = int(valueR2)
-s = ""
-if(x == 0):
-    s = "00000000"
-while(x > 0):
-    if(x % 2 == 1):
-        s = "1" + s
+
+def b_leftshift(user):
     
-    else:
-        s = "0" + s
+    val=l.op_code["ls"]
+    valueR1=l.op_code[user[1]]
+    valueR2=user[2][1:]
+    x = int(valueR2)
+    s = ""
+    if(x == 0):
+        s = "00000000"
+    while(x > 0):
+        if(x % 2 == 1):
+            s = "1" + s
     
-    x //= 2
+        else:   
+            s = "0" + s
+    
+        x //= 2
 
-y = 8 - len(s)
-temp = ""
-while(y):
-    temp += "0"
-    y -=1
+    y = 8 - len(s)
+    temp = ""
+    while(y):
+        temp += "0"
+        y -=1
 
-s = temp + s
+    s = temp + s
 
-print(val+valueR1+s)
+    return(val+valueR1+s)
