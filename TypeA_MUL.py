d9f913c85f8403c72fa6d6edb4019eee2ffd0991
sejalkhurana21418 <108174729+sejalkhurana21418@users.noreply.github.com>
2022-07-03 01:36:02 +0530

    Update TypeA_MUL.py

--- TypeA_MUL.py
+++ TypeA_MUL.py
@@ -1,9 +1,11 @@
 import DICT_VALUE as l
 import Unused_bit as U
 
-user=[x for x in input().split()]
-val=l.op_code["mul"]
-valueR1=l.op_code[user[1]]
-valueR2=l.op_code[user[2]]
-valueR3=l.op_code[user[3]]
-print(val+U.unused["A"]+valueR1+valueR2+valueR3)
+def mul():
+  
+  user=[x for x in input().split()]
+  val=l.op_code["mul"]
+  valueR1=l.op_code[user[1]]
+  valueR2=l.op_code[user[2]]
+  valueR3=l.op_code[user[3]]
+  return (val+U.unused["A"]+valueR1+valueR2+valueR3)
