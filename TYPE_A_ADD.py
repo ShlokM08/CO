6b11411993cfed0d41b90f63042065f1353361e3
ShlokM08 <108210012+ShlokM08@users.noreply.github.com>
2022-07-02 17:02:19 +0530

    ENTER

--- TYPE_A_ADD.py
+++ TYPE_A_ADD.py
@@ -1,12 +1,13 @@
 import DICT_VALUE as value
 import Unused_bit as U
+import STDIN as stdin
 #add R1 R2 R3
 #opcode=5bits unused=3bits reg1=3bits reg2=3bits reg3=3bits
-to_encode=[x for x in input().split()]
-def add(to_encode):
+
+def add():
     binary_encoding=""
-    if to_encode[0]=="add":
+    if stdin.to_read[0]=="add":
         binary_encoding=value.op_code["add"]
-    binary_encoding+=U.unused["A"]+value.op_code[to_encode[1]]+value.op_code[to_encode[2]]+value.op_code[to_encode[3]]
+    binary_encoding+=U.unused["A"]+value.op_code[stdin.to_read[1]]+value.op_code[stdin.to_read[2]]+value.op_code[stdin.to_read[3]]
     print(binary_encoding)
-add(to_encode)
+add(stdin.to_read)
