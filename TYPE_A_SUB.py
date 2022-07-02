39dea2569d30564a3874f63a26a927163c3faf87
varsha21297 <varsha21297@iiitd.ac.in>
2022-07-02 21:28:50 +0530

    Merge branch 'master' of https://github.com/ShlokM08/CO

--- TYPE_A_SUB.py
+++ TYPE_A_SUB.py
@@ -1,6 +1,6 @@
 import DICT_VALUE as d
 import Unused_bit as ub
-def a_sub(inp):
+def A_sub(inp):
     s=d.op_code['sub']+ub.unused["A"]+d.op_code[inp[1]]+d.op_code[inp[2]]+d.op_code[inp[3]]
     return s
 
--- TYPE_C_NOT.py
+++ TYPE_C_NOT.py
@@ -1,6 +1,6 @@
 import DICT_VALUE as d
 import Unused_bit as ub
 inp=[i for i in input().split()]
-def c_not(inp):
+def C_not(inp):
     s=d.op_code['not']+ub.unused['C']+d.op_code[inp[1]]+d.op_code[inp[2]]
     return s
