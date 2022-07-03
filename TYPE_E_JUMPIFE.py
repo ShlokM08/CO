a7ba459bd38fb9b54865c00386f48267bcf7c476
ShlokM08 <108210012+ShlokM08@users.noreply.github.com>
2022-07-03 21:48:30 +0530

    master

--- TYPE_E_JUMPIFE.py
+++ TYPE_E_JUMPIFE.py
@@ -3,7 +3,7 @@ import Unused_bit as U
 
 
 def E_jumpife(user, d):
-    val=l.op_code["jgt"]
+    val=l.op_code["je"]
     for i in d.keys():
         if i==user[1]:
             s=d[i]
--- TYPE_E_UNCONDITIONALJUMP.py
+++ TYPE_E_UNCONDITIONALJUMP.py
@@ -2,7 +2,7 @@ import DICT_VALUE as l
 import Unused_bit as U
 
 def E_u_jump(user, d): 
-    val=l.op_code["jgt"]
+    val=l.op_code["jmp"]
     for i in d.keys():
         if i==user[1]:
             s=d[i]
