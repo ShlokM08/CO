c9ce9b9458af5835d7b19a513ed115029529ec21
varsha21297 <varsha21297@iiitd.ac.in>
2022-07-03 16:07:56 +0530

    enter

--- STDIN1.py
+++ STDIN1.py
@@ -47,43 +47,86 @@ for j in l:
     #print(j[0])
     if j[0]=="add":
         print(A_add(j))
+        x=A_add(j)
+        lst.append(x)
     elif j[0]=="sub":
         print(A_sub(j))
+        x=A_sub(j)
+        lst.append(x)
     elif j[0]=="not":
         print(C_not(j))
+        x=C_not(j)
+        lst.append(x)
     elif j[0]=="and":
         print(A_and(j))
+        x=A_and(j)
+        lst.append(x)
     elif j[0]=="or":
         print(A_or(j))
+        x=A_or(j)
+        lst.append(x)
     elif j[0]=="xor":
         print(A_xor(j))
+        x=A_xor(j)
+        lst.append(x)
     elif j[0]=="div":
         print(C_div(j))
+        x=C_div(j)
+        lst.append(x)
     elif j[0]=="hlt":
         print(F_hlt(j))
+        x=F_hlt(j)
+        lst.append(x)
     elif j[0]=="rs":
         print(B_rs(j))
+        x=B_rs(j)
+        lst.append(x)
     elif j[0]=="mov":
         if j[2]=="R1" or j[2]=="R2" or j[2]=="R3":
             print(C_move_R(j))
+            x=C_move_R(j)
         elif '$' in j[2]:
             print(B_mov_i(j))
+            x=B_mov_i(j)
+        lst.append(x)
     elif j[0]=='st':
         print(D_store(j))
+        x=D_store(j)
+        lst.append(x)
     elif j[0]=='mul':
         print(A_mul(j))
+        x=A_mul(j)
+        lst.append(x)
     elif j[0]=='cmp':
         print(C_compare(j))
+        x=C_compare(j)
+        lst.append(x)
     elif j[0]=='jlt':
         print(E_jump_less(j))
+        x=E_jump_less(j)
+        lst.append(x)
     elif j[0]=='ls':
         print(B_leftshift(j))
+        x=B_leftshift(j)
+        lst.append(x)
     elif j[0]=='ld':
         print(D_load(j))
+        x=D_load(j)
+        lst.append(x)
     elif j[0]=='jmp':
         print(E_u_jump(j))
+        x=E_u_jump(j)
+        lst.append(x)
     elif j[0]=='jgt':
         print(E_jumpifg(j))
+        x=E_jumpifg(j)
+        lst.append(x)
     elif j[0]=='je':
         print(E_jumpife(j))
-        
+        x=E_jumpife(j)
+        lst.append(x)
+
+with open('OUTPUT.TXT', 'w') as f:
+    for line in lst:
+        line=line+'\n'
+        f.write(line)  
