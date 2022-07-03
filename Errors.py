481ed5caf87f95f9fa9d07213a242c56b179a5b4
varsha21297 <varsha21297@iiitd.ac.in>
2022-07-03 22:01:15 +0530

    enter

--- Errors.py
+++ Errors.py
@@ -1,13 +1,9 @@
 
-import DICT_VALUE as d 
-import STDIN1
-
+import DICT_VALUE as d
 
 def keypresent(j):
-   
     for each in j:
-
-        if each not in d:
+        if each not in d.op_code.keys():
             print("Typos in instruction name or register name")
             return False
     
