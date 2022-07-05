e12b2d9f75a9a0c6ea48b8495bf1b4c6c729a8d7
ShlokM08 <108210012+ShlokM08@users.noreply.github.com>
2022-07-05 12:58:48 +0530

    master

--- Memory_Address.py
+++ Memory_Address.py
@@ -1,30 +1,4 @@
-#var_list=['abc', 'yoo', 'tyi']
-'''def mem_add(total_lines,number_of_var,var_list):
-    for i in range(0,len(var_list)):
-        var_mem_address=var_list[i]
-        #print(var_mem_address)
-        var_mem_address=total_lines-number_of_var
-        
-        total_lines+=7
-        return var_mem_address 
-    #
-
-#print(mem_add(7,4,var_list))'''
-
-L=["hlt","var x","mov R1 R2 R3"]
-l1=[]
-for i in L:
-    x=i.split(" ")
-    l1.append(x)
-print(l1)
-for i in l1:
-    if "var" not in l1[0]:
-        print("ERROR")
-        break
-    
-
-'''for i in L:
-    if "var" in L[i].split(" "):
-        print("yes")
-    elif 'var' not in L:
-        print("NO")'''
+a=["label:"]
+n=len(a[0])
+print(a[0][n-1:n])
+print(a[0][0:n-1])
