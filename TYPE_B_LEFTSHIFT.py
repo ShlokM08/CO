7bd25b4ea8267a52850b16776d2ff3330e45347f
varsha21297 <varsha21297@iiitd.ac.in>
2022-07-06 23:31:13 +0530

    enter

--- TYPE_B_LEFTSHIFT.py
+++ TYPE_B_LEFTSHIFT.py
@@ -6,17 +6,10 @@ def B_leftshift(user):
     val=l.op_code["ls"]
     valueR1=l.op_code[user[1]]
     valueR2=user[2][1:]
-    with open("TO_READ.txt") as f:
-        file_read=f.read().split('\n')
-        for i in range(len(file_read)):
-            if valueR2 in file_read[i]:
-                i+=1
-                break
-    num=len(file_read)-i
     mem_addr=''
     while num:
         r=num%2
-        mem_addr+=str(r)
+        mem_addr+=str(valueR2)
         num//=2
     mem_addr=mem_addr[::-1]
     if len(mem_addr)<8:
