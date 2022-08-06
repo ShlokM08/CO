58b6440f74cec0d882f0a3146bb25bb58bd96089
ShlokM08 <108210012+ShlokM08@users.noreply.github.com>
2022-08-06 22:51:06 +0530

    enter

--- Q5_BONUS.py
+++ Q5_BONUS.py
@@ -44,7 +44,7 @@ def type_1():
         n_reg=2**reg_len
         print(f"It can support a maximum of {n_reg} instructions")
         addr_type=int(input("Enter your choice:"))
-    elif addr_type==2:
+    if addr_type==2:
         nibble= math.log(int(4)) / math.log(2)
         addr_pins=val_inp-nibble
         print(f"We need minimum {addr_pins} bits to represent an address in this architecture")
@@ -57,7 +57,7 @@ def type_1():
         n_reg=2**reg_len
         print(f"It can support a maximum of {n_reg} instructions")
         addr_type=int(input("Enter your choice:"))
-    elif addr_type==3:
+    if addr_type==3:
         nibble= math.log(int(8)) / math.log(2)
         addr_pins=val_inp-nibble
         print(f"We need minimum {addr_pins} bits to represent an address in this architecture")
@@ -70,7 +70,7 @@ def type_1():
         n_reg=2**reg_len
         print(f"It can support a maximum of {n_reg} instructions")
         addr_type=int(input("Enter your choice:"))
-    elif addr_type==4:
+    if addr_type==4:
         cpu_size=int(input("Enter cpu_size size:"))
         cpu= math.log(int(cpu_size)) / math.log(2)
         addr_pins=val_inp-cpu
@@ -85,100 +85,102 @@ def type_1():
         print(f"It can support a maximum of {n_reg} instructions")
         addr_type=int(input("Enter your choice:"))
 
-    elif addr_type>4:
+    if addr_type>4:
         print("Thank You")
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 #---------------------------------------------------------TYPE2------------------------------------------------------------------------------------------
 def type_2():
-    bit_conversion={"kb":2**10,"mb":2**20,"gb":2**30} 
-    cpu_size=int(input("Enter CPU size"))
-    addr_pins=int(input("Enter number of address pins"))
+    #bit_conversion={"kb":2**10,"mb":2**20,"gb":2**30} 
+    cpu_size=int(input("Enter CPU size:"))
+    addr_pins=int(input("Enter number of address pins:"))
     print("1. Bit Addressable Memory - Cell Size = 1 bit")
     print("2. Nibble Addressable Memory - Cell Size = 4 bit")
     print("3. Byte Addressable Memory - Cell Size = 8 bits(standard)")
     print("4. Word Addressable Memory - Cell Size = Word Size (depends on CPU)")
 
     addr_type=int(input("Enter your choice:"))
+    def expo(x):
+        p=0
+        while(x!=1):
+            x=x/2
+            p+=1
+        return p
+
+    if addr_type ==1:
+        if addr_pins>=30:
+            reg_num=addr_pins-30
+            reg_num=reg_num-3
+            print(2**reg_num,"GB")
+        if addr_pins>=20 and addr_pins<30:
+            reg_num=addr_pins-20
+            reg_num=reg_num-3
+            print(2**reg_num,"MB")
+        if addr_pins>=10 and addr_pins<20:
+            reg_num=addr_pins-10
+            reg_num=reg_num-3
+            print(2**reg_num,"B")
+        #addr_type = int(input("Enter your choice: "))
     if addr_type==2:
-        final_answer=""
-        storage=(2**addr_pins)*(2**2)
-        #print(storage)
-        cpu= math.log(int(storage)) / math.log(2)
-        print(cpu)
-        z=int(cpu)%10  #/10=3.7
-        if cpu-z==30:
-           x="GB"
-        elif cpu-z==20:
-           x ="MB"
-        elif cpu-z==10:
-            x="KB"
-        else:
-            print("Incorrect Value")
-        print(z)
-        final_val=z-2
-        print(final_val)
-        fin=2**final_val
-        print(fin)
-        final_answer+=str(fin)+" "+x
-        print(final_answer)
-        addr_type=int(input("Enter your choice:"))
-
+        if addr_pins>=30:
+            addr_pins+=2
+            reg_num=addr_pins-30
+            reg_num=reg_num-3
+            print(2**reg_num,"GB")
+        if addr_pins>=20 and addr_pins<30:
+            addr_pins+=2
+            reg_num=addr_pins-20
+            reg_num=reg_num-3
+            print(2**reg_num,"MB")
+        if addr_pins>=10 and addr_pins<20:
+            addr_pins+=2
+            reg_num=addr_pins-10
+            reg_num=reg_num-3
+            print(2**reg_num,"B")
+        #addr_type = int(input("Enter your choice: "))
     if addr_type==3:
-        final_answer=""
-        storage=(2**addr_pins)*(2**3)
-        cpu= math.log(int(storage)) / math.log(2)
-        z=int(cpu)%10  #/10=3.7
-        if cpu-z==30:
-           x="GB"
-        elif cpu-z==20:
-           x ="MB"
-        elif cpu-z==10:
-            x="KB"
-        else:
-            print("Incorrect Value")
-        final_val=z-3
-        fin=2**final_val
-        final_answer+=str(fin)+" "+x
-        print(final_answer)
-        addr_type=int(input("Enter your choice:"))
-        
+        if addr_pins>=30:
+            addr_pins+=3
+            reg_num=addr_pins-30
+            reg_num=reg_num-3
+            print(2**reg_num,"GB")
+        if addr_pins>=20 and addr_pins<30:
+            addr_pins+=3
+            reg_num=addr_pins-20
+            reg_num=reg_num-3
+            print(2**reg_num,"MB")
+        if addr_pins>=10 and addr_pins<20:
+            addr_pins+=3
+            reg_num=addr_pins-10
+            reg_num=reg_num-3
+            print(2**reg_num,"B")
+        #addr_type = int(input("Enter your choice: "))
+    if addr_type==4:
+        if addr_pins>=30:
+            addr_pins+=expo(cpu_size)
+            reg_num=addr_pins-30
+            reg_num=reg_num-3
+            print(2**reg_num,"GB")
+        if addr_pins>=20 and addr_pins<30:
+            addr_pins+=expo(cpu_size)
+            reg_num=addr_pins-20
+            reg_num=reg_num-3
+            print(2**reg_num,"MB")
+        if addr_pins>=10 and addr_pins<20:
+            addr_pins+=expo(cpu_size)
+            reg_num=addr_pins-10
+            reg_num=reg_num-3
+            print(2**reg_num,"B")
+        #addr_type = int(input("Enter your choice: "))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if addr_type>4:
+        print("Thank You")
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 
-type=int(input("Input the type you want"))
-if type==1:
+Type=int(input("Input the type you want:"))
+if Type==1:
     type_1()
-elif type==2:
+elif Type==2:
     type_2()
 else:
     print("Enter valid type")
